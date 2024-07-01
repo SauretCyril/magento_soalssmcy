776885e32ef71a1a8477f93896bf580185f87470
khameds <khameds@gmail.com>
2024-07-01 16:59:32 +0200

    Add : import faker lib, define commands

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   env: {
     // variable environnement
+    checkoutBaseUrl : "https://magento.softwaretestingboard.com/checkout/"
   },
   e2e: {
     setupNodeEvents(on, config) {
