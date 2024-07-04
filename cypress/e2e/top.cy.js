8edc2faa8c6795e562c36225d686ed51e7b27f94
Sonakhatchatrian <sona83xy@yahoo.fr>
2024-07-04 15:53:29 +0200

    ajout wait

--- cypress/e2e/top.cy.js
+++ cypress/e2e/top.cy.js
@@ -8,6 +8,7 @@ describe('selected tops', () => {
     cy.get(".product-image-photo").first().click()
     cy.url().should("include", "breathe-easy-tank.html")
     cy.get(".page-title").should("contain", "Breathe-Easy Tank")
+    cy.wait(6000)
     cy.get("#option-label-size-143-item-166").click().should("have.css","backgroundColor","rgb(255, 255, 255)")
     
   
