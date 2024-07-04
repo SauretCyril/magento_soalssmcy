b8acb899d93d319de06a8a4093e0890c599fe7db
Lilicoco42 <alicia.lacote@laposte.net>
2024-07-04 12:24:21 +0200

    Update and rename cypress/e2e/auth.cy.js to cypress/tops.cy.js

---
+++ cypress/tops.cy.js
@@ -0,0 +1,15 @@
+describe('selected tops', () => {
+  it('select', () => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.get('#ui-id-4').trigger('mouseover')
+    cy.get(".level1").first().click()
+    cy.url().should("include","women/tops-women.html")
+    cy.get(".page-title").should("contain", "Tops")
+    cy.get(".product-image-photo").first().click()
+    cy.url().should("include", "breathe-easy-tank.html")
+    cy.get(".page-title").should("contain", "Breathe-Easy Tank")
+    cy.get("#option-label-size-143-item-166").click().should("have.css","backgroundColor","rgb(255, 255, 255)")
+    
+  
+  })
+})
